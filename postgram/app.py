24f49f4272adfc9614cd268757397a470ead54c1
Rohit Sankaran <rohit.sankaran@gmail.com>
2013-01-14 23:55:18 -0600

    Initial attempt at getting data

--- postgram/app.py
+++ postgram/app.py
@@ -1,6 +1,16 @@
+import json
+import os
 from random import random
 
-def get_lat_long():
+from flask import *
+import requests
+
+from env import *
+
+app = Flask(__name__)
+
+
+def get_lat_lng():
     multiplier = 10000;
     latitude=(random()*(90*multiplier))/multiplier
     longitude=(random()*(180*multiplier))/multiplier
@@ -15,3 +25,27 @@ def get_lat_long():
 
     return latitude,longitude
 
+
+def get_data():
+    lat,lng = get_lat_lng()
+    auth = FOAUTH_USER, FOAUTH_PASSWORD
+    data = {'distance': 5000, 'lat': lat, 'lng': lng}
+    r = requests.get(FOAUTH_URL, params=data, auth=auth)
+    return r
+
+
+@app.route('/')
+def index():
+    r = get_data()
+    if r.status_code == 200 and len(r.text) > 0:
+        resp_dict = json.loads(r.text)
+        return str(resp_dict)
+    else:
+        return ''
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
