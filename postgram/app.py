7a83ba89ecf245965c16d6588f14fcadec2142de
Rohit Sankaran <rohit.sankaran@gmail.com>
2013-01-15 23:37:48 -0600

    Added instagram image link

--- postgram/app.py
+++ postgram/app.py
@@ -31,17 +31,21 @@ def get_data():
     auth = FOAUTH_USER, FOAUTH_PASSWORD
     data = {'distance': 5000, 'lat': lat, 'lng': lng}
     r = requests.get(FOAUTH_URL, params=data, auth=auth)
-    return r
+    return r, lat, lng
 
 
 @app.route('/')
 def index():
-    r = get_data()
+    r, lat, lng = get_data()
     if r.status_code == 200 and len(r.text) > 0:
         resp_dict = json.loads(r.text)
-        return str(resp_dict)
+        gurl = '<a href="https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q={lat}+,+{lng}&vpsrc=0&ie=UTF8&t=m&z=3&iwloc=near">Google Maps</a>'.format(lat=lat,lng=lng)
+        try:
+            return '<img src="{0}"/><br>{1}<br>{2}<br>{3}'.format(resp_dict['data'][0]['images']['standard_resolution']['url'], resp_dict['data'][0]['location'], resp_dict['data'][0]['link'], gurl)
+        except (KeyError, IndexError):
+            return str(resp_dict) + str(lat) + ' , ' + str(lng) + '\n' + gurl
     else:
-        return ''
+        return '{0}-{1}'.format(r.status_code, r.text)
 
 
 if __name__ == '__main__':
